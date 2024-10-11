4c87fc34cf007d8bc4ded0eb5b7294d1e22f04cd
Meldurnar <osduran@ucm.es>
2024-10-11 18:09:00 +0200

    Añadido game.js e importado phaser

---
+++ docs/game.js
@@ -0,0 +1,27 @@
+import MenuScene from "./MenuScene.js";
+/**
+ * Inicio del juego en Phaser. Creamos el archivo de configuración del juego y creamos
+ * la clase Game de Phaser, encargada de crear e iniciar el juego.
+ */
+let config = {
+    type: Phaser.AUTO,
+    width:  800,
+    height: 600,
+    /**
+     * @type {boolean}
+     */
+    pixelArt: true,
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
+    },
+    scene: [MenuScene],    // Decimos a Phaser cual es nuestra escena
+    physics: { 
+        default: 'arcade', 
+        arcade: { 
+            gravity: { y: 200 }, 
+            debug: false 
+        } 
+    }
+};
+
+new Phaser.Game(config);
